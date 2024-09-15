cebc149a190a4add961cda97a03f4dbf2fb38f1f
Cristina <avicon95@protonmail.com>
2024-09-15 18:28:41 -0500

    Card linear color and shadow corrected

--- components/alerts/AlertCard.tsx
+++ components/alerts/AlertCard.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Patient } from "@/types/patient";
 import { Theme } from "@/constants/theme";
+import { LinearGradient } from 'expo-linear-gradient';
 
 interface PatientCardProps {
   name: Patient["name"];
@@ -16,6 +17,10 @@ const AlertCard: React.FC<PatientCardProps> = ({
 }) => {
   return (
     <View style={styles.card}>
+      <LinearGradient
+      colors={['#0A7AD6', '#0A63AC']}
+      style={styles.background}
+    ></LinearGradient>
       <Text style={styles.patientName}>Pac. {name}</Text>
       <View style={styles.infoContainer}>
         <View style={styles.info}>
@@ -35,17 +40,24 @@ export default AlertCard;
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: Theme.colors.blue,
-    borderRadius: Theme.borderRadius,
+    flex: 1,
+    minHeight: 120, 
     paddingVertical: Theme.padding.vertical,
     paddingHorizontal: Theme.padding.horizontal,
     // Shadow for iOS
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.4,
-    shadowRadius: 2,
     // Shadow for Android
     elevation: 5,
+  },
+  background: {
+    borderRadius: Theme.borderRadius,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
   },
   patientName: {
     color: Theme.colors.white,
