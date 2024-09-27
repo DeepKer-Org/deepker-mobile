9664fecee85f96fcb6ebb171b9896c540602b4ac
Cristina <avicon95@protonmail.com>
2024-09-26 22:38:42 -0500

    Add Log out button on information page

--- app/(tabs)/general.tsx
+++ app/(tabs)/general.tsx
@@ -1,21 +1,33 @@
 import InfoElement from "@/components/general/InfoElement";
+import Button from "@/components/ui/Button";
 import { Theme } from "@/constants/theme";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function Tab() {
   return (
     <View style={styles.background}>
-      <View style={styles.userContainer}>
-        <Text style={styles.h1}>Información General</Text>
-        <InfoElement label={"Nombre"} value={"Juan Carlos"} />
-        <InfoElement label={"Puesto"} value={"Enfermero"} />
-        <InfoElement label={"Área"} value={"Cardiología"} />
-        <InfoElement label={"Centro"} value={"Hosp. de las Esperanzas"} />
-        <InfoElement label={"Horario"} value={"LV 8:00 AM a 5:00 PM"} lastElement />
-      </View>
-      <View style={styles.systemContainer}>
-        <InfoElement label={"Nombre del Sistema"} value={"DeepKer"}/>
-        <InfoElement label={"Versión"} value={"1.0.0"} lastElement />
+      <View style={styles.container}>
+        <View>
+          <View style={styles.userContainer}>
+            <Text style={styles.h1}>Información General</Text>
+            <InfoElement label={"Nombre"} value={"Juan Carlos"} />
+            <InfoElement label={"Puesto"} value={"Enfermero"} />
+            <InfoElement label={"Área"} value={"Cardiología"} />
+            <InfoElement label={"Centro"} value={"Hosp. de las Esperanzas"} />
+            <InfoElement
+              label={"Horario"}
+              value={"LV 8:00 AM a 5:00 PM"}
+              lastElement
+            />
+          </View>
+          <View style={styles.systemContainer}>
+            <InfoElement label={"Nombre del Sistema"} value={"DeepKer"} />
+            <InfoElement label={"Versión"} value={"1.0.0"} lastElement />
+          </View>
+        </View>
+        <View>
+          <Button text={"CERRAR SESIÓN"} onPress={() => {}} />
+        </View>
       </View>
     </View>
   );
@@ -26,19 +38,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Theme.colors.whiteBlue,
   },
-  userContainer: {
+  container: {
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "space-between",
     marginHorizontal: Theme.margin.horizontal,
-    marginVertical: Theme.margin.vertical
+    paddingVertical: Theme.padding.vertical
+  },
+  userContainer: {
+    marginVertical: Theme.margin.vertical,
   },
   systemContainer: {
-    marginHorizontal: Theme.margin.horizontal,
-    marginVertical: Theme.margin.vertical * 1.5
+    marginVertical: Theme.margin.vertical * 1.5,
   },
   h1: {
     fontFamily: Theme.fonts.semibold,
     color: Theme.colors.black,
     fontSize: Theme.size.h1,
-    marginBottom: Theme.margin.vertical * 1.5
+    marginBottom: Theme.margin.vertical * 1.5,
   },
-}
-);
+});
